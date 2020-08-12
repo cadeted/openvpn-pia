1614676e6feb1f7799526aeed228324352872a09
cadeted <59097143+cadeted@users.noreply.github.com>
2020-08-11 23:14:44 -0500

    Update iproute.sh

--- iproute.sh
+++ iproute.sh
@@ -8,4 +8,5 @@ sleep 15  # Ensure tunnel creation is complete
 if [ -n "${LOCAL_NETWORK}" ]; then
     eval "$(ip r l | grep -v 'tun0\|kernel'|awk '{print "GW="$3"\nINT="$5}')"
     ip route add "$LOCAL_NETWORK" via "$GW" dev "$INT"
+    echo "$LOCAL_NETWORK" via "$GW" dev "$INT"
 fi
