b31fcf605f9f0af0417a3f0f0e1e08bc2ac2b1a6
cadeted <59097143+cadeted@users.noreply.github.com>
2020-08-15 16:17:08 -0500

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -25,9 +25,9 @@ set -- "$@" '--auth-nocache'
 set -- "$@" '--status' '/var/log/openvpn.status'
 
 #set ip route
-if [ -n "${LOCAL_NETWORK}" ]; then
+if [ -n "${LOCAL_NETWORK:-}" ]; then
     eval "$(ip r l | grep -v 'tun0\|kernel'|awk '{print "GW="$3"\nINT="$5}')"
-    ip route add "$LOCAL_NETWORK" via "$GW" dev "$INT"
+    ip route add "${LOCAL_NETWORK}" via "$GW" dev "$INT"
 fi
 
 #run openvpn
